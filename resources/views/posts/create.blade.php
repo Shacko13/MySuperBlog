312ec2bc6b0b051c3a2a7dba0bd81523ac428584
Todd Szakacs <tszakacs@msn.com>
2018-01-25 15:30:50 -0500

    work on posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,13 +15,18 @@
     			<label for="title">Title:</label>
     			<input type="text" class="form-control" id="title" name="title">
   			</div>
+
   			<div class="form-group">
    			    <label for="body">Body</label>
     			<textarea id="body" name="body" class="form-control"></textarea>
   			</div>
-  			<button type="submit" class="btn btn-primary">Publish</button>
-		</form>
 
+  			<div class="form-group">
+  				<button type="submit" class="btn btn-primary">Publish</button>		
+			</div>
+			
+			@include ('layouts.errors')
+		</form>
 	</div>
 
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,42 +3,18 @@
 @section ('content')
 
 	<div class="col-sm-8 blog-main">
+        
+        @foreach ($posts as $post)
 
-          <div class="blog-post">
-            <h2 class="blog-post-title">Sample blog post</h2>
-            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            @include('posts.post')
+                
+        @endforeach
 
-            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-            <hr>
-           
-          </div><!-- /.blog-post -->
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">Another blog post</h2>
-            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-
-            <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-            <blockquote>
-          </div><!-- /.blog-post -->
-
-          <div class="blog-post">
-            <h2 class="blog-post-title">New feature</h2>
-            <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
-
-            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <ul>
-              <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-              <li>Donec id elit non mi porta gravida at eget metus.</li>
-              <li>Nulla vitae elit libero, a pharetra augue.</li>
-            </ul>
-            
-          </div><!-- /.blog-post -->
-
-          <nav class="blog-pagination">
+        <nav class="blog-pagination">
             <a class="btn btn-outline-primary" href="#">Older</a>
             <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-          </nav>
+        </nav>
 
-        </div>
+    </div>
 
 @endsection
